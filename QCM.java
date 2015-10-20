5c0a28474ae161b752f032a85dc12dd0bc25f03d
Gauthier Fossion <gauthier.fossion@student.uclouvain.be>
2015-10-20 09:36:04 +0000

    QCM.java : Définition des tâches & du planning

--- QCM.java
+++ QCM.java
@@ -7,10 +7,11 @@
 public class QCM
 {
 
-// Récuperer les données depuis le fichier.
+// Récuperer les données depuis le fichier txt.
 // Variable pour les questions : string :  question1,2,3,4,5,6,...
 // Variable pour les réponses : string : reponse1a, reponse1b, reponse1c, ...
 // Variable pour la bonne réponse : string : repC : variable de la bonne réponse
+// --> Melvin : 21/10/2015 : Soir
 
 // Méthode de correction :  
 // Demander à l'utilisateur : Combien vaut 1 bonne réponse, combien vaut une mauvaise réponse, combien vaut une réponse blanche.
@@ -19,6 +20,7 @@ public class QCM
 // Variable pour la mauvaise réponse : repM : double
 // Variable pour la réponse blanche : repN : double
 // Variable pour le nombre de points total : pointsTotal : int
+// --> Julien : 20/10/2015 : Soir
 
 // PRNG -> Math.random
 // PRNG doit sortir un nombre aléatoire entre 1 & le nombre max de question sans répétitions.
@@ -29,7 +31,7 @@ public class QCM
 // Incrémentation des bonnes réponses suivant la variable repB
 // Incrémentation des mauvaises réponses : repM 
 // Incrémenation des réponses blanches : repN
-
+// --> Gauthier : 22/10/2015 : soir
 
 // Affichage des résultats : 
 // Score final : XX / XX
@@ -37,9 +39,8 @@ public class QCM
 // Score final : Nombre de bonnes réponses
 // Score final : Nombres de réponses blanches
 // Affichage des réponses fausses avec correction
+// --> Gauthier : 22/10/2015 : Soir
 
 // Sauvegarde des résultats dans un fichier texte.
-
-
-
+// --> Melvin : 23/10/2015 : Soir
 }
