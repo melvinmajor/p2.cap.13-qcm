ae27cc84f3b83d04f3e7f9a25af8860feb5941cb
melvinmajor <melvin.campos@outlook.be>
2015-10-28 16:10:30 +0100

    J'ai rien compris mais j'ai modifié les erreurs accents, etc.

--- AssessmentFormConsole.java
+++ AssessmentFormConsole.java
@@ -8,13 +8,13 @@ import mcq.AssessmentForm;
 import util.PRNG;
 
 /**
- * Un simple simulateur de questionnaire à choix multiple fonctionnant en mode console
+ * Un simple simulateur de questionnaire a choix multiple fonctionnant en mode console
  * Vous pouvez modifier ce fichier pour votre projet ou vous en inspirer librement.
- * Comme vous pouvez voir nous fournissons à titre d'exemple une simple methode d'évaluation (simpleGrader).
- * Les questions sont toujours présentées dans le même ordre ainsi que les propositions de réponses.
- * N'hésitez pas à implémenter d'autres évaluateurs et ajouter de l'aléatoire dans la présentation/selection des questions
+ * Comme vous pouvez voir nous fournissons a titre d'exemple une simple methode d'evaluation (simpleGrader).
+ * Les questions sont toujours presentees dans le meme ordre ainsi que les propositions de reponses.
+ * N'hesitez pas a implementer d'autres evaluateurs et ajouter de l'aleatoire dans la presentation/selection des questions
  * et des choix.
- * @author Gauthier Fossion, Melvin Campos, Julien Pluquet
+ * @author Gauthier Fossion, Melvin Campos Casares, Julien Pluquet
  */
 public final class AssessmentFormConsole 
 {
@@ -23,23 +23,23 @@ public final class AssessmentFormConsole
             // Affiche la question
             System.out.println(q.getWording());
         
-            // Récupération du tableau des choix possibles 
+            // Recuperation du tableau des choix possibles 
             Choice [] choices = q.getChoices();
             
-            // On itère sur les choix (ici aussi on pourrait le faire dans un ordre aléatoire)
+            // On itère sur les choix (ici aussi on pourrait le faire dans un ordre aleatoire)
             for (int i = 0; i < choices.length; i++) 
                 {
                     Choice choice = choices[i];
-                    // Affichage du choix à l'indice i
+                    // Affichage du choix a l'indice i
                     System.out.println("["+i+"]\t"+choice);
                 }
-            // On propose à l'utilisateur de ne pas répondre
-            System.out.println("["+choices.length+"]\tJe ne désire pas répondre");
+            // On propose a l'utilisateur de ne pas repondre
+            System.out.println("["+choices.length+"]\tJe ne desire pas repondre");
       
-            System.out.println("Please, input an answer and press Enter ...");
+            System.out.println("SVP, ins�rez une r�ponse et appuyer sur Entrer...");
       
-            // on demande à l'étudiant de taper au clavier sa réponse (un entier parmi les choix possibles)
-            // ceci est equivalent à TextIO.getIntLn() vu au cours avec Olivier Bonaventure et Charles Pecheur
+            // on demande a l'etudiant de taper au clavier sa reponse (un entier parmi les choix possibles)
+            // ceci est equivalent a TextIO.getIntLn(), vu au cours avec Olivier Bonaventure et Charles Pecheur
             Scanner in = new Scanner(System.in);
             return in.nextInt();
         }
@@ -50,13 +50,13 @@ public final class AssessmentFormConsole
             
             // On demande les points du QCM
             Scanner saisieUtilisateur = new Scanner(System.in);
-            System.out.println("Sur combien de points le QCMM doit-il être ?");
+            System.out.println("Sur combien de points le QCM doit-il etre ?");
             double pointsTotal = saisieUtilisateur.nextInt();
-            System.out.println("Combien vaut une bonne réponse ?");
+            System.out.println("Combien vaut une bonne reponse ?");
             double repB = saisieUtilisateur.nextDouble(); 
-            System.out.println("Combien vaut une mauvaise réponse ?");
+            System.out.println("Combien vaut une mauvaise reponse ?");
             double repM = saisieUtilisateur.nextDouble(); 
-            System.out.println("Combien vaut une réponse blanche ?");
+            System.out.println("Combien vaut une reponse blanche ?");
             double repN = saisieUtilisateur.nextDouble(); 
             
             // On les affiches
@@ -66,19 +66,19 @@ public final class AssessmentFormConsole
             System.out.println(repN);
             
             String filename = "QCM.txt";
-            // Charchement du QCM depuis le fichier (questions et réponses aux questions)
+            // Chargement du QCM depuis le fichier (questions et reponses aux questions)
             AssessmentForm mcq = AssessmentFormLoader.buildQuestionnaire(filename);
 
-            // Recupération du tableau des questions
+            // Recuperation du tableau des questions
             Question [] questions = mcq.getQuestions();
         
             double totalScore = 0.0;
-            // On itère sur chaque question pour evalue le score de l'étudiant à cette question
+            // On itère sur chaque question pour evalue le score de l'etudiant a cette question
             // Suggestion: on pourrait iterer dans un ordre aleatoire ou sur un sous ensemble aleatoire des questions
             for (int i = 0; i < questions.length; i++) 
             {
                 Question q = questions[i];
-                //getAnswer(q) te renvoie la réponse entrée par l'utilisateur.
+                //getAnswer(q) te renvoie la reponse entree par l'utilisateur.
                 int reponseUtilisateur = getAnswer(q);
                 Choice [] choices = q.getChoices();
                 
@@ -99,11 +99,11 @@ public final class AssessmentFormConsole
                     }
                 
                 // A toi de jouer mon ami ! 
-                totalScore = 0.0; // A compléter
+                totalScore = 0.0; // A completer
             }
       
             // affichage du score final
-            System.out.println("your score is:"+totalScore);
+            System.out.println("Votre score est:"+totalScore);
 
     }
 
